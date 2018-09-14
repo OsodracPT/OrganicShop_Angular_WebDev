6a0690f83a6fdf85c08e24d52c9a0ba2ee4d04d0
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-09-14 23:37:32 +0100

    Login and LogOut

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  user: firebase.User;
 
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) { 
+    afAuth.authState.subscribe( x=> this.user = x);
   }
 
+
+  logout()
+  {
+  this.afAuth.auth.signOut();
+  }
 }
