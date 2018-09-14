ee8965c40e7ae0bfdc659a681a69f3d0e206ce68
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-09-15 00:11:15 +0100

    Login fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,7 +9,7 @@ import * as firebase from 'firebase';
 })
 export class LoginComponent {
 
-  constructor(public afAuth: AngularFireAuth) { 
+  constructor(private afAuth: AngularFireAuth) { 
   }
 
   login() { 
