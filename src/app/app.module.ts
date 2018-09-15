050beb76fa3296cef08faea3710f3dd3809f7c80
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-09-15 17:35:30 +0100

    Removed Firebase
    Causing to much issues with packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { LoginComponent } from './login/login.component';
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
-import {AngularFireModule} from 'angularfire2';
-import {AngularFireDatabaseModule} from 'angularfire2/database';
-import {AngularFireAuthModule} from 'angularfire2/auth';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
@@ -35,9 +32,6 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
   ],
   imports: [
     BrowserModule, 
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent},
