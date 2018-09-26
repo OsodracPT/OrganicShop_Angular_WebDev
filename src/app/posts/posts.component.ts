ae4539b587d747dd2f760546886cc9f256c439be
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-09-26 21:02:28 +0100

    Deleting Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -36,6 +36,14 @@ export class PostsComponent implements OnInit {
     this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true}))
     .subscribe(response => {
       console.log(response.json());
+    });
+  }
+
+  deletePost(post){
+    this.http.delete(this.url + '/' + post.id)
+    .subscribe(response => {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index, 1);
     })
   }
 
