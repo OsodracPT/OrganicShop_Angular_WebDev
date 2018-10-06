c7ef97ece2e78e2394e845b53778a437b088ff0a
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-10-06 16:01:52 +0100

    Redirecting Users After Logging In

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,6 +1,6 @@
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +11,11 @@ export class AuthGuardService implements CanActivate {
     private router: Router,
     private authService: AuthService) { }
 
-  canActivate(){
+  canActivate(route, state: RouterStateSnapshot){
     if(this.authService.isLoggedIn())
     return true;
 
-    this.router.navigate(['/login']);
+    this.router.navigate(['/login'], {queryParams: {returnUrl: state.url }});
     return false;
   }
 }
